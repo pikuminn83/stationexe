c00c3a00d3df2883ffc389f032a906e2dd4e4b95
pikuminn83 <105100893+pikuminn83@users.noreply.github.com>
2022-11-01 18:23:57 +0900

    シーン移動のプログラムを作成
    最初の場所のモデル完成

---
+++ Unity Project stationexe/Assets/script/NextStage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextStage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("a");
+        Invoke("ChangeScene",0.5f);
+    }
+
+    // Update is called once per frame
+    void ChangeScene()
+    {
+          Debug.Log("b");
+        SceneManager.LoadScene("StationGameStage2");
+    }
+}
